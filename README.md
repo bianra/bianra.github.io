35b9e5acae657a2c91e39a534319686eae3101e4
bjw_BJW <bjwbrlql907011@gmail.com>
2026-03-30 15:02:31 +0800

    Delete all files

--- README.md
+++
@@ -1,1 +0,0 @@
-# bianra.github.io
--- core/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('delete/<int:pk>/', views.delete_message, name='delete_message'),
-    path('like/<int:pk>/', views.like_message, name='like_message'),
-]
